ad30866bacb07ecc2ca417377eaaa41dad84a282
Jose D. Masa <jdmasa@gmail.com>
2024-03-24 19:43:05 +0100

    Initial commit

---
+++ app.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type hourlyprice []struct {
+	Day   string  `json:"day"`
+	Hour  int     `json:"hour"`
+	Price float64 `json:"price"`
+	Zone  string  `json:"zone"`
+}
+
+func main() {
+
+	// Get request
+	resp, err := http.Get("https://raw.githubusercontent.com/jorgeatgu/apaga-luz/main/public/data/today_price.json")
+	if err != nil {
+		fmt.Println("No response from request")
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body) // response body is []byte
+
+	var today hourlyprice
+	if err := json.Unmarshal(body, &today); err != nil { // Parse []byte to the go struct pointer
+		fmt.Println("Can not unmarshal JSON")
+	}
+
+	// Get request
+	resp2, err := http.Get("https://raw.githubusercontent.com/jorgeatgu/apaga-luz/main/public/data/tomorrow_price.json")
+	if err != nil {
+		fmt.Println("No response from request")
+	}
+	defer resp.Body.Close()
+	body2, _ := io.ReadAll(resp2.Body) // response body is []byte
+
+	var tomorrow hourlyprice
+	if err := json.Unmarshal(body2, &tomorrow); err != nil { // Parse []byte to the go struct pointer
+		fmt.Println("Can not unmarshal JSON")
+	}
+
+	// fmt.Println(PrettyPrint(result))
+
+	hours, _, _ := time.Now().Clock()
+	// Loop through the data node for the FirstName
+	for _, rec := range today {
+		if rec.Hour >= hours {
+			fmt.Println(rec.Hour)
+			fmt.Println(rec.Zone)
+		}
+
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module precioluz
+
+go 1.19
+
+require github.com/alexellis/blinkt_go v0.0.0-20180120180744-cc0ca163e0bc
+
+require (
+	github.com/alexellis/rpi v0.0.0-20170116141016-ab6a4e79f0bd // indirect
+	github.com/rogpeppe/rog-go v0.0.0-20210831093509-3c886aea54f1 // indirect
+)
